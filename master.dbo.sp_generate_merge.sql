c0086eec569b706279d285216e3a3c70eb8f65f3
Basil <basillea@hotmail.com>
2022-03-09 16:31:17 +0100

    Fix typo

--- master.dbo.sp_generate_merge.sql
+++ master.dbo.sp_generate_merge.sql
@@ -41,7 +41,7 @@ CREATE PROC [sp_generate_merge]
  @include_rowsaffected bit = 1, -- When 1, a section is added to the end of the batch which outputs rows affected by the MERGE
  @nologo bit = 0, -- When 1, the "About" comment is suppressed from output
  @batch_separator nvarchar(50) = 'GO', -- Batch separator to use. Specify NULL to output all statements within a single batch
- @output nvarchar(max), = null output -- Use this output parameter to return the generated T-SQL batches to the caller (Hint: specify @batch_separator=NULL to output all statements within a single batch)
+ @output nvarchar(max) = null output, -- Use this output parameter to return the generated T-SQL batches to the caller (Hint: specify @batch_separator=NULL to output all statements within a single batch)
  
  -- Customisations
  @generate_updatevalues bit = 1, 
